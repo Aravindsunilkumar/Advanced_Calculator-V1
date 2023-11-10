3ad83389865f531145c41f05ff7ef290ccd85708
Aravind Sunil Kumar <Aravindsunilkumar@users.noreply.github.com>
2023-11-10 21:44:48 +0100

    Update Math.py

--- Math.py
+++ Math.py
@@ -1,12 +1,2 @@
-def math(arg1,arg2,arg3,arg4):
-    if (arg1 == True):
-        input=int(input('Enter the value A'))
-    else:
-        input=int(input('Enter the value '))
-    if (arg2 == True):
-        input=int(input('Enter the value B'))
-    if (arg3 == True):
-        input=int(input('Enter the value C'))
-    if (arg4 == True):
-        input=int(input('Enter the value D'))
+
  
